886243aef999fbfe6ab854d1c5943b8c50aa96d4
Robson Calheira <151081433+Robdev01@users.noreply.github.com>
2024-06-26 20:32:49 -0300

    Update Program.cs

--- CSHARP/01_Conceitos/Projeto05.operadores/Program.cs
+++ CSHARP/01_Conceitos/Projeto05.operadores/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,10 +11,22 @@ namespace Projeto05.operadores
         static void Main(string[] args)
         {
             // Operador de divisão inteira
-            Console.Write("Informe um número inteiro:");
+            // Console.Write irá mostra na tela.
 
+            Console.Write("Informe um número inteiro: ");
+            // int.Parse irá converter a string para Int
             int n1 = int.Parse(Console.ReadLine());
 
+            Console.Write("Informe outro número inteiro: ");
+
+            int n2 = int.Parse(Console.ReadLine());
+            // para devolver decimal é só colocar o Double e para mostra inteiro coloca int 
+            Double resultado =(Double) n1 / (Double)n2;
+
+            Console.Write($"Divisão inteira: {resultado} ");
+            // esse Console.ReadKey ira rodar o app
+            Console.ReadKey();
         }
     }
 }
+
