ae6cfe7f9a13674823f5f50f3bbb6f8c73cc2a91
Robson Calheira <151081433+Robdev01@users.noreply.github.com>
2024-06-26 21:38:46 -0300

    Add files via upload

---
+++ CSHARP/01_Conceitos/Projeto06.OperadorTernario/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto06.OperadorTernario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solicitar a idade de uma pessoa
+            //Console.Write("Informe sua idade: ");
+            //int idade = int.Parse(Console.ReadLine());
+
+            //string resposta = idade < 18 ? "Menor de Idade" : "Maior de Idade";
+
+            //Console.WriteLine($"Você é {resposta}");
+            Console.WriteLine("Em que ano estamos");
+
+            int ano = int.Parse(Console.ReadLine());
+
+            string diasFevereiro = ano % 4 == 0 ? "Não bissexto" :"Bissexto";
+
+            Console.WriteLine($"O ano {ano} é {diasFevereiro} ");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
